cd8fc3b3dcbd6913b4c4259f0afd2f447e4e8a01
Rebekah Jones <jones.rebekah17@gmail.com>
2019-09-24 18:02:11 -0500

    Update manager_info.sql

--- manager_info.sql
+++ manager_info.sql
@@ -1,7 +1,7 @@
 --List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, last name, first name, and start and end employment dates
 
-SELECT dm.dept_no, dm.emp_no, dm.from_date, dm.to_date, dp.dept_name, emp.first_name, emp.last_name
+SELECT emp.first_name, emp.last_name, dm.dept_no, dp.dept_name, dm.emp_no, dm.from_date, dm.to_date
 FROM dept_manager AS dm
 	INNER JOIN departments AS dp 
 		ON (dm.dept_no = dp.dept_no)
